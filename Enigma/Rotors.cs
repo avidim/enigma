472cad3679cb220cc14ff7beb18b2291b69df6a2
Javid <javidgasimzada@gmail.com>
2021-02-27 18:10:25 +0400

    Remove ignored files

---
+++ Enigma/Rotors.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma
+{
+    class Rotors
+    {
+        private string keys;
+
+        public Rotors(int num)
+        {
+            switch (num)
+            {
+                case 1: keys = "EKMFLGDQVZNTOWYHXUSPAIBRCJ"; break;
+                case 2: keys = "AJDKSIRUXBLHWTMCQGZNPYFVOE"; break;
+                case 3: keys = "BDFHJLCPRTXVZNYEIWGAKMUSQO"; break;
+            }
+        }
+
+        public char getCharacter(int id)
+        {
+            return keys[id];
+        }
+
+        public int getCharacterID(char key)
+        {
+            return keys.IndexOf(key);
+        }
+    }
+}
